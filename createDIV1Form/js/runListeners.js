6d887d7810eb2376ebdc364f11e6ff0f64ccb04a
CreateDiv <git@bronzecooley.com>
2025-05-21 13:01:20 -0400

    Update runListeners.js

--- createDIV1Form/js/runListeners.js
+++ createDIV1Form/js/runListeners.js
@@ -1,7 +1,7 @@
  document.getElementById('cancel').addEventListener('click',function(event){
   event.preventDefault();
   document.getElementById('bodyColor').hidden = true;
-  document.getElementById('chat').hidden = true;
-  document.getElementById('openMain').hidden = false;
+  document.getElementById('bcc_chatDIV').hidden = true;
+  document.getElementById('bcc_openChat').hidden = false;
   console.log("Executed a cancel of the form window");
  });
