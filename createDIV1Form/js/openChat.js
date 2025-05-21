46e9bbbe373a324f75d71fc44484d33ae85c6085
CreateDiv <git@bronzecooley.com>
2025-05-21 13:00:05 -0400

    Update openChat.js

--- createDIV1Form/js/openChat.js
+++ createDIV1Form/js/openChat.js
@@ -1,7 +1,7 @@
   document.getElementById('bcc_openChat').addEventListener('click',function(event){
    event.preventDefault();
    document.getElementById('bodyColor').hidden = false;
-   document.getElementById('chat').hidden = false;
-   document.getElementById('openMain').hidden = true;
+   document.getElementById('bcc_chatDIV').hidden = false;
+   document.getElementById('bcc_openChat').hidden = true;
    console.log("Found the end");
  });
