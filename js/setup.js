1bf0934c47d0804c98872c1b9b1cdb0d99d94766
CreateDiv <git@bronzecooley.com>
2025-05-21 09:02:42 -0400

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -1,16 +1,29 @@
-function setup(){
-  const createDiv = createElement('formPoint');
-  createDiv.id('formPoint');
+  /* Used to create the point in which form will live, body of the page */
+  const createDiv = createElement('div');
+  createDiv.id('bcc_formDIV');
   document.body.appendChild(createDiv);
-  
+
+  /* Used to create the point in which chat will live, body of the page */
+  const createDiv = createElement('div');
+  createDiv.id('bcc_chatDIV');
+  createDiv.hidden = true;
+  document.body.appendChild(createDiv);
+
+  /* Button used to intitiate the form, body of the page */
   const initialButton = createElement('button');
-  initialButton.id = 'openMain';
-  
-  document.head.appendChild(bronzeStylesheet.css);
-  document.body.appendChild('php/chat.php');
+  initialButton.id = 'bcc_openChat';
+  document.body.appendChild(initialButton);
 
+  /* Adding the form to the page */
+  fetch('php/chat.php')
+    .then(response => response.text())
+    .then(data => {
+       document.getElementById('bcc_formDIV').innerHTML = data;
+    })
+    .catch(error => {
+       console.error('Error loading file:', error);
+  });
 
-  
-}
+  /* It's own stylesheet  */
+  document.head.appendChild(bcc_stylesheet.css);
 
-setup()
