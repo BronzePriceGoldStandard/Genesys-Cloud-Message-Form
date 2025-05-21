2c48daa89c151bfe54c3044f63e60bdaa6f84eca
BronzePriceGoldStandard <git@bronzecooley.com>
2025-05-21 08:30:50 -0400

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,16 @@
 function setup(){
+  const createDiv = createElement('formPoint');
+  createDiv.id('formPoint');
+  document.body.appendChild(createDiv);
+  
+  const initialButton = createElement('button');
+  initialButton.id = 'openMain';
+  
   document.head.appendChild(bronzeStylesheet.css);
   document.body.appendChild('php/chat.php');
+
+
+  
 }
 
 setup()
