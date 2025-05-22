444f74bca380fba63ab74bd2813f9a87c74e2554
Bronze Cooley <git@bronzecooley.com>
2025-05-22 12:56:54 -0400

    Update runListeners.js

--- createDIV1Form/js/runListeners.js
+++ createDIV1Form/js/runListeners.js
@@ -2,6 +2,6 @@
   event.preventDefault();
   document.getElementById('bodyColor').hidden = true;
   document.getElementById('bcc_chatDIV').hidden = true;
-  document.getElementById('bcc_openChat').hidden = false;
+  document.getElementById('chat-button').hidden = false;
   console.log("Executed a cancel of the form window");
  });
